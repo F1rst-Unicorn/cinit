6f2a3eac73d39cb7b79c97c12c84ca1a53939e97
Jan Veen <jan.veen@posteo.de>
2018-11-20 17:37:42 +0100

    Issue #7: Move dependency generation to dep manager

--- src/runtime/dependency_graph.rs
+++ src/runtime/dependency_graph.rs
@@ -1,4 +1,7 @@
 use std::collections::VecDeque;
+use std::collections::HashMap;
+
+use config::config::Config;
 
 
 /// Process information relevant for dependency resolution
@@ -19,7 +22,8 @@ pub struct DependencyManager {
 
 impl DependencyManager {
 
-    pub fn with_nodes(nodes: Vec<ProcessNode>) -> Self {
+    pub fn with_nodes(config: &Config, name_dict: &HashMap<String, usize>) -> Self {
+        let nodes = DependencyManager::build_dependencies(config, name_dict);
         DependencyManager {
             runnable: DependencyManager::find_initial_runnables(&nodes),
             nodes,
@@ -40,6 +44,7 @@ impl DependencyManager {
             let mut successor = &mut self.nodes[successor_index];
             successor.predecessor_count -= 1;
             if successor.predecessor_count == 0 {
+                // no need to remove `process` from successor's dependencies
                 self.runnable.push_back(successor_index);
                 result.push(successor_index);
             }
@@ -54,6 +59,62 @@ impl DependencyManager {
                 result.push_back(i);
             }
         }
+        result
+    }
+
+    fn build_dependencies(config: &Config, name_dict: &HashMap<String, usize>) -> Vec<ProcessNode> {
+        let mut result = Vec::with_capacity(config.programs.len());
+
+        for _ in 0..config.programs.len() {
+            result.push(ProcessNode {
+                before: Vec::new(),
+                predecessor_count: 0,
+            });
+        }
+
+        for process_config in &config.programs {
+            let current_index = name_dict
+                .get(&process_config.name)
+                .expect("Invalid index in name_dict")
+                .clone();
+            {
+                let mut current = result
+                    .get_mut(current_index)
+                    .expect("Invalid index in name_dict");
+                for predecessor_name in &process_config.before {
+                    let predecessor_index = name_dict
+                        .get(predecessor_name)
+                        .expect("Invalid index in name_dict")
+                        .clone();
+                    current.before.push(predecessor_index);
+                }
+
+                current.predecessor_count += process_config.after.len();
+            }
+
+            for predecessor_name in &process_config.before {
+                let predecessor_index = name_dict
+                    .get(predecessor_name)
+                    .expect("Invalid index in name_dict")
+                    .clone();
+                let mut predecessor = result
+                    .get_mut(predecessor_index)
+                    .expect("Invalid index in name_dict");
+                predecessor.predecessor_count += 1;
+            }
+
+            for predecessor in &process_config.after {
+                let dependency_index = name_dict
+                    .get(predecessor)
+                    .expect("Invalid index in name_dict")
+                    .clone();
+                let mut dependency = result
+                    .get_mut(dependency_index)
+                    .expect("Invalid index in name_dict");
+                dependency.before.push(current_index);
+            }
+        }
+
         result
     }
 }
--- src/runtime/process_manager_builder.rs
+++ src/runtime/process_manager_builder.rs
@@ -5,7 +5,7 @@ use std::process::exit;
 
 use config::config::Config;
 use runtime::process::{Process};
-use runtime::dependency_graph::{DependencyManager, ProcessNode};
+use runtime::dependency_graph::DependencyManager;
 
 use nix::sys::signalfd;
 
@@ -17,9 +17,7 @@ impl ProcessManager {
 
         let name_dict = ProcessManager::build_name_dict(&processes);
 
-        let nodes = ProcessManager::build_dependencies(&config, &name_dict);
-
-        let dependency_manager = DependencyManager::with_nodes(nodes);
+        let dependency_manager = DependencyManager::with_nodes(&config, &name_dict);
 
         ProcessManager {
             processes,
@@ -41,62 +39,6 @@ impl ProcessManager {
                 exit(2);
             } else {
                 result.insert(desc.name.to_owned(), i);
-            }
-        }
-
-        result
-    }
-
-    fn build_dependencies(config: &Config, name_dict: &HashMap<String, usize>) -> Vec<ProcessNode> {
-        let mut result = Vec::with_capacity(config.programs.len());
-
-        for _ in 0..config.programs.len() {
-            result.push(ProcessNode {
-                before: Vec::new(),
-                predecessor_count: 0,
-            });
-        }
-
-        for process_config in &config.programs {
-            let current_index = name_dict
-                .get(&process_config.name)
-                .expect("Invalid index in name_dict")
-                .clone();
-            {
-                let mut current = result
-                    .get_mut(current_index)
-                    .expect("Invalid index in name_dict");
-                for predecessor_name in &process_config.before {
-                    let predecessor_index = name_dict
-                        .get(predecessor_name)
-                        .expect("Invalid index in name_dict")
-                        .clone();
-                    current.before.push(predecessor_index);
-                }
-
-                current.predecessor_count += process_config.after.len();
-            }
-
-            for predecessor_name in &process_config.before {
-                let predecessor_index = name_dict
-                    .get(predecessor_name)
-                    .expect("Invalid index in name_dict")
-                    .clone();
-                let mut predecessor = result
-                    .get_mut(predecessor_index)
-                    .expect("Invalid index in name_dict");
-                predecessor.predecessor_count += 1;
-            }
-
-            for predecessor in &process_config.after {
-                let dependency_index = name_dict
-                    .get(predecessor)
-                    .expect("Invalid index in name_dict")
-                    .clone();
-                let mut dependency = result
-                    .get_mut(dependency_index)
-                    .expect("Invalid index in name_dict");
-                dependency.before.push(current_index);
             }
         }
 
