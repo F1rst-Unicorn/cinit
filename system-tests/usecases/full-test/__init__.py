32c1629aa512cd312a33aaa69f0a83d652b9df98
Jan Veen <jan.veen@posteo.de>
2018-11-22 15:48:45 +0100

    Reformat full test
    Remove error code from process
    Reformat function calls

--- system-tests/usecases/full-test/__init__.py
+++ system-tests/usecases/full-test/__init__.py
@@ -6,20 +6,28 @@ class Test(CinitTest):
     def test(self):
         self.run_cinit(self.get_test_dir(__file__))
 
-        self.assert_on_trace()                                              \
-                .that(Sequential(
-                        ChildSpawned("first"),
-                        ChildExited("first")))                              \
-                .then(Parallel(
-                        Sequential(
-                                ChildSpawned("second"),
-                                ChildExited("second")),
-                        Sequential(
-                                ChildSpawned("third"),
-                                ChildCrashed("third", 42))))                \
-                .then(Sequential(
-                        ChildSpawned("fourth"),
-                        ChildExited("fourth")))                             \
+        self.assert_on_trace().that(
+            Sequential(
+                ChildSpawned("first"),
+                ChildExited("first")
+            )
+        ).then(
+            Parallel(
+                Sequential(
+                    ChildSpawned("second"),
+                    ChildExited("second")
+                ),
+                Sequential(
+                    ChildSpawned("third"),
+                    ChildExited("third")
+                )
+            )
+        ).then(
+            Sequential(
+                ChildSpawned("fourth"),
+                ChildExited("fourth")
+            )
+        )
 
 
         ChildProcess("first", self)\
